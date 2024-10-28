9b5290aa7de2202715570b3f8a3f41ed3debab3d
Mr Footwork <pandau.ting@gmail.com>
2024-10-28 23:31:59 +0100

    iteration 6: alphabetic order

--- src/movies.js
+++ src/movies.js
@@ -46,7 +46,12 @@ function orderByYear(moviesArray) {
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+	return [...moviesArray]
+		.map(({ title }) => title)
+		.sort()
+		.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
