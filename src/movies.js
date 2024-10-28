422bc03c4d7682fd8ef7cda0e5e8027d0799a2ab
Mr Footwork <pandau.ting@gmail.com>
2024-10-28 23:25:48 +0100

    iteration 5: order by year

--- src/movies.js
+++ src/movies.js
@@ -39,7 +39,11 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+	return [...moviesArray].sort((a, b) =>
+		a.year === b.year ? a.title.localeCompare(b.title) : a.year - b.year
+	);
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
